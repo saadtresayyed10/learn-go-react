7e783e087df9add545212376951a6dc81a38dee6
Saad Sayyed <saadsyed950@gmail.com>
2024-10-29 20:00:10 +0530

    Delete func done

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
-	// app.Delete("/api/todos/:id", deleteTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
 
@@ -126,4 +126,21 @@ func updateTodo(c *fiber.Ctx) error {
 
 }
 
-// func deleteTodo(c *fiber.Ctx) error {}
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid Todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+
+}
