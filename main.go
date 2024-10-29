b8f60d60f48400f1ddae8c20299923474faca0a5
Saad Sayyed <saadsyed950@gmail.com>
2024-10-29 19:49:46 +0530

    Update func done

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
-	// app.Patch("/api/todos/:id", updateTodo)
+	app.Patch("/api/todos/:id", updateTodo)
 	// app.Delete("/api/todos/:id", deleteTodo)
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
@@ -106,5 +106,24 @@ func createTodo(c *fiber.Ctx) error {
 	return c.Status(201).JSON(todo)
 }
 
-// func updateTodo(c *fiber.Ctx) error {}
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid TODO id"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return nil
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": "updated successfully"})
+
+}
+
 // func deleteTodo(c *fiber.Ctx) error {}
