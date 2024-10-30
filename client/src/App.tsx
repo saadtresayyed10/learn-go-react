ad832fd5438fc0472ec9401f9283b3c5a2edaee6
Saad Sayyed <saadsyed950@gmail.com>
2024-10-30 12:39:49 +0530

    Changing urls for dev n prod

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import Navbar from "./_components/Navbar";
 import TodoForm from "./_components/TodoForm";
 import TodoList from "./_components/TodoList";
 
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 const App = () => {
   return (
     <Stack h="100vh">
